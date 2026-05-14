ba7a36ac50faba98520695ae4ca8150b565fcb41
Wilson Fabio Flores Taboada <wilson8548125@gmail.com>
2026-05-14 16:16:21 -0400

    header

--- resources/views/components/header-component.blade.php
+++ resources/views/components/header-component.blade.php
@@ -477,49 +477,36 @@
     </div>
 </div>
 <!-- FIN NOTIFICACIONES -->
-                <div class="dropdown ms-sm-3 header-item topbar-user">
+                  <div class="dropdown ms-sm-3 header-item topbar-user">
                     <button type="button" class="btn" id="page-header-user-dropdown" data-bs-toggle="dropdown"
                         aria-haspopup="true" aria-expanded="false">
                         <span class="d-flex align-items-center">
-                            <img class="rounded-circle header-profile-user" src="@if (Auth::user()->avatar != ''){{ URL::asset('images/' . Auth::user()->avatar) }}@else{{ URL::asset('assets/images/users/avatar-1.jpg') }}@endif"
-                                alt="Header Avatar">
+
+                            <img class="rounded-circle header-profile-user" src="{{ auth()->user()->img ? asset('storage/' . auth()->user()->img) : asset('assets/images/users/deafult-user.jpg') }}"
+                                alt="Imagen del Usuario">
                             <span class="text-start ms-xl-2">
-                                <span class="d-none d-xl-inline-block ms-1 fw-medium user-name-text">{{Auth::user()->name}}</span>
-                                <span class="d-none d-xl-block ms-1 fs-12 text-muted user-name-sub-text">Founder</span>
+                                <span class="d-none d-xl-inline-block ms-1 fw-medium user-name-text">{{auth()->user()->name}}</span>
+                                <span class="d-none d-xl-block ms-1 fs-12 text-muted user-name-sub-text text-center">
+                                    {{ auth()->user()->roles->isNotEmpty() ? auth()->user()->roles->first()->name : 'Sin rol asignado' }}
+                                </span>
                             </span>
                         </span>
                     </button>
                     <div class="dropdown-menu dropdown-menu-end">
                         <!-- item-->
-                        <h6 class="dropdown-header">Welcome Anna!</h6>
-                        <a class="dropdown-item" href="pages-profile"><i
+                        <h6 class="dropdown-header">{{auth()->user()->name}}!</h6>
+                        {{-- <a class="dropdown-item" href="pages-profile"><i
                                 class="mdi mdi-account-circle text-muted fs-16 align-middle me-1"></i> <span
-                                class="align-middle">Profile</span></a>
-                        <a class="dropdown-item" href="apps-chat"><i
-                                class="mdi mdi-message-text-outline text-muted fs-16 align-middle me-1"></i> <span
-                                class="align-middle">Messages</span></a>
-                        <a class="dropdown-item" href="apps-tasks-kanban"><i
-                                class="mdi mdi-calendar-check-outline text-muted fs-16 align-middle me-1"></i> <span
-                                class="align-middle">Taskboard</span></a>
-                        <a class="dropdown-item" href="pages-faqs"><i
-                                class="mdi mdi-lifebuoy text-muted fs-16 align-middle me-1"></i> <span
-                                class="align-middle">Help</span></a>
-                        <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="pages-profile"><i
-                                class="mdi mdi-wallet text-muted fs-16 align-middle me-1"></i> <span
-                                class="align-middle">Balance : <b>$5971.67</b></span></a>
-                        <a class="dropdown-item" href="pages-profile-settings"><span
-                                class="badge bg-soft-success text-success mt-1 float-end">New</span><i
+                                class="align-middle">Perfil</span></a>
+                        <div class="dropdown-divider"></div> --}}
+
+                        {{-- <a class="dropdown-item" href="pages-profile-settings"><i
                                 class="mdi mdi-cog-outline text-muted fs-16 align-middle me-1"></i> <span
-                                class="align-middle">Settings</span></a>
-                        <a class="dropdown-item" href="auth-lockscreen-basic"><i
-                                class="mdi mdi-lock text-muted fs-16 align-middle me-1"></i> <span class="align-middle">Lock screen</span></a>
-                        <a class="dropdown-item " href="javascript:void();"
-                            onclick="event.preventDefault(); document.getElementById('logout-form').submit();"><i
-                                class="bx bx-power-off font-size-16 align-middle me-1"></i> <span
-                                key="t-logout">@lang('translation.logout')</span></a>
-                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                class="align-middle">Configuracion</span></a> --}}
+                        <form action="{{route('logout')}}" method="post">
                             @csrf
+                            <button class="dropdown-item "><i class="bx bx-power-off font-size-16 align-middle me-1"></i>
+                            <span key="t-logout">Cerrar Sesion</span></button>
                         </form>
                     </div>
                 </div>
